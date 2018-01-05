748486c456840018fda1c48aef3840d61847deb8
ardittot <atrianggada@microservices-explore.c.virtual-cycling-188314.internal>
2018-01-05 04:18:37 +0000

    edit command.sh

--- command.sh
+++ command.sh
@@ -1,8 +1,16 @@
+# Install all required GO packages
 go get -u github.com/gin-gonic/gin
 go get -u github.com/Shopify/sarama
-go run main.go
+
+# Compile & run
+#go run main.go
 go build
 ./kafka-pubsub
+
+# Preparation
+##Edit kafka-sarama.go
+##(1) Variable brokers to point to desired Kafka broker clusters
+##(2) Function useConsumer():  to customize your desired commands for consumed data from Kafka
 
 # Add new topic for kafka consumer to be running forever as a goroutine
 curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"topic":"test2","group":"test2-group1"}' http://localhost:8010/subscribe/add
